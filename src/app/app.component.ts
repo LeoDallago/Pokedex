0c2d0c8679c110b7a634f989cb887b8d314d297a
LeoDallago <leodallago@outlook.com>
2024-09-24 15:46:15 -0300

    implementado conexao com a api

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,36 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NavbarComponent } from "./components/navbar/navbar.component";
+import { PokeApiService } from './services/poke-api.service';
+import { Pokemon } from './models/pokemon';
+import { NgForOf } from '@angular/common';
+import { converterParaTitleCase } from './util/converter-para-title-case';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [NavbarComponent],
+  imports: [NavbarComponent, NgForOf],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
-export class AppComponent {
-  title = 'pokedex';
+export class AppComponent implements OnInit {
+  public pokemons: Pokemon[];
+
+  constructor(private pokeApiService: PokeApiService) {
+    this.pokemons = [];
+  }
+
+  ngOnInit(): void {
+    this.pokeApiService.selecionarTodos().subscribe((res) => {
+      const arrayDeResultados = res.results as any[];
+
+      console.log(arrayDeResultados);
+
+      this.pokemons = arrayDeResultados.map(obj => {
+        return {
+          nome: converterParaTitleCase(obj.name),
+        }
+      });
+    });
+  }
 }
